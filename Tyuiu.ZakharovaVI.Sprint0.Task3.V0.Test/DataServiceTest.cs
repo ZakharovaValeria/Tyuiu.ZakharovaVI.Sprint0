8c7ccf64dfd9f6424680fa8b224721e19e591dc5
ZakharovaValeria <Valeria122610Z@yandex.ru>
2025-09-13 21:58:10 +0500

    Task3

---
+++ Tyuiu.ZakharovaVI.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ZakharovaVI.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.ZakharovaVI.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DaraServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
