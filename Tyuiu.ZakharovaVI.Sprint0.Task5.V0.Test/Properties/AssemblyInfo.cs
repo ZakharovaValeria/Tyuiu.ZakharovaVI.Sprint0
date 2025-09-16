743fbecdbc9ce6539eaccfb1de7b408d72cff8fa
ZakharovaValeria <Valeria122610Z@yandex.ru>
2025-09-16 20:12:09 +0500

    Task5

---
+++ Tyuiu.ZakharovaVI.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZakharovaVI.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZakharovaVI.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b503a77d-7137-4df3-868d-c8af3720f7e0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ZakharovaVI.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Tyuiu.ZakharovaVI.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.ZakharovaVI.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("А + Б = " + DataService.Addition(1, 5));
+            Console.WriteLine("А - Б = " + DataService.Subtraction(15, 5));
+            Console.WriteLine("А * Б = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("А / Б = " + DataService.Division(9, 3));
+            Console.ReadKey();
+        }
+    }
+}
